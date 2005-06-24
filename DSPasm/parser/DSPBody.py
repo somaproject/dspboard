3d47b0d0929b55903611cd99ffb651929f99105d
/C=US/ST=Massachusetts/O=Massachusetts Institute of Technology/OU=Client CA v1/CN=Matthew W Fishburn/emailAddress=fishburn@MIT.EDU </C=US/ST=Massachusetts/O=Massachusetts Institute of Technology/OU=Client CA v1/CN=Matthew W Fishburn/emailAddress=fishburn@MIT.EDU@5dccc5d9-beb7-0310-bf71-cc18d937a3f4>
2005-06-24 11:43:36 +0000

    Reformatted body parser code.
    git-svn-id: file:///home/jonas/soma/svn-restore.20080710/DSPboard/trunk@984 5dccc5d9-beb7-0310-bf71-cc18d937a3f4

--- DSPasm/parser/DSPBody.py
+++ DSPasm/parser/DSPBody.py
@@ -3,7 +3,6 @@
 from mx.TextTools import *
 
 head_pos = 0
-
 def jump_count(taglist,txt,l,r,subtag):
     global head_pos
     if head_pos is None:
@@ -14,93 +13,129 @@ def jump_count(taglist,txt,l,r,subtag):
     else:
         head_pos = r
 
-labels = []
+################# body -> statements parser #######################
 
-label_parser = (labels, AppendToTag+Table,
-                (#check for non-white-space
-                 (None,AllIn,alphanumeric+'._'),
-                 (None,Is,':'),
-                 (None,IsIn, white+newline),
-                 (None,Skip, -1)
-                 ))
+def tokensFromBody(body_str):
+    """
+    Input:  A string containing assembly code
+    Output: a tuple of
+            (statements, comments, labels, includes)
+    """
+    labels = []
+    label_parser = (labels, AppendToTag+Table,
+                    (#check for non-white-space
+        (None,AllIn,alphanumeric+'._'),
+        (None,Is,':'),
+        (None,IsIn, white+newline),
+        (None,Skip, -1)
+        ))
 
-statements = []
-
-statement_parser = (statements, AppendToTag+Table,)
-
-includes = []
-
-include_parser = \
-      (includes, AppendToTag+Table,
-       (#check for #
+    includes = []
+    include_parser = \
+                   (includes, AppendToTag+Table,
+                    (#check for #
         (None,IsIn,'''.#'''),
         (None,AllNotIn,newline),
         (None,AllIn,newline),
         (None, Skip, -1)
         ))
 
-comments = []
-
-comment_parser = \
-      (comments, AppendToTag+Table,
-       (#check for //
+    comments = []
+    comment_parser = \
+                   (comments, AppendToTag+Table,
+                    (#check for //
         (None,Word,'''//'''),
         (None,AllNotIn,newline),
         (None,AllIn,newline),
         (None, Skip, -1)
         ))
+    
+    statements = []
+    statement_parser = \
+                     (statements, AppendToTag+Table,
+                      (#check for alphanumeric, run until hit a , or ;
+        #need to recurse to do paren matching :>
+        (None,IsIn,a2z+A2Z),
+        (None,AllNotIn,';,('),
+        (None,IsIn,';,',+1,+10),
+        (None,AllNotIn,')',-10,-2)))
 
-statements = []
+    tag_table = (
+        include_parser+(+1,0),
+        comment_parser+(+1,-1),
+        label_parser+(+1,-2),
+        statement_parser+(+1,-3),
+        (None,AllIn,white+newline,+1,-4),
+        (None,IsNotIn,white+newline,+1), # uninteresting
+        (jump_count, Skip+CallTag, 0),      # Check for infinite loop
+        (None,EOF,Here,-7)) # EOF
 
-statement_parser = \
-       (statements, AppendToTag+Table,
-        (#check for alphanumeric, run until hit a , or ;
-         #need to recurse to do paren matching :>
-         (None,IsIn,a2z+A2Z),
-         (None,AllNotIn,';,('),
-         (None,IsIn,';,',+1,+10),
-         (None,AllNotIn,')',-10,-2)))
+    result, taglist, nextindex = tag(body_str,tag_table,0,len(body_str))
 
-#(tagobj, command+flags, command_argument, [,jump_no_match] [,jump_match=+1])
-tag_table = (
-             include_parser+(+1,0),
-             comment_parser+(+1,-1),
-             label_parser+(+1,-2),
-             statement_parser+(+1,-3),
-             (None,AllIn,white+newline,+1,-4),
-             (None,IsNotIn,white+newline,+1), # uninteresting
-             (jump_count, Skip+CallTag, 0),      # Check for infinite loop
-             (None,EOF,Here,-7)) # EOF
+    return (statements,comments,labels,includes)
 
-set = []
+################# statement -> set, queried variables parser
 
-eq_set = (set,AppendToTag+Table,
-                 (
-    (None,AllNotIn,';=\n'),
-    (None,Is,'='),
-    (None,Skip,-1)), +2, +1)
+def varsFromStatement(st_str=''):
+    """
+    Input: a single assembly statement
+    Output: a tuple ofset and queried variables in the statement
+            (set, queried)
 
-qur=[]
+    varsFromStatement('r0 = r1') -> (['r0'],['r1'])
+    """
+    set = []
+    qur=[]
 
-eq_qur = (qur,AppendToTag+Table,
-          (
-    (None,Is,'='),
-    (None,AllNotIn,',;'+newline),
-    (None,AllIn,',;'+newline)),+1,-3)
+    eq_set = (set,AppendToTag+Table,
+              (
+        (None,AllNotIn,'='),
+        (None,Is,'='),
+        (None,Skip,-1)), +2, +1)
 
- #for parsing statements
-statement_table = ((None,Word,'''rts''',+1,0),
-                   (None,Table,((None,Word,'''if'''),
-                                (None,AllIn,white),
-                                (None,AllIn,alpha+'_.'),
-                                (None,AllIn,white))
-                                            ,+1,-1),
-                   eq_set,
-                   eq_qur,
-                   (None,IsNotIn,'',-4),
-                   (None,EOF,Here,-5),
-                   )
+    eq_qur = (qur,AppendToTag+Table,
+              (
+        (None,Is,'='),
+        (None,AllNotIn,',;'+newline),
+        (None,AllIn,',;'+newline)),+1,-3)
 
+    statement_table = ((None,Word,'''rts''',+1,0),
+                       (None,Table,((None,Word,'''if'''),
+                                    (None,AllIn,white),
+                                    (None,AllIn,alpha+'_.'),
+                                    (None,AllIn,white))
+                        ,+1,-1),
+                       eq_set,
+                       eq_qur,
+                       (None,IsNotIn,'',-4),
+                       (None,EOF,Here,-5),
+                       )
+
+    st_result, st_taglist, st_nextindex = tag(st_str,statement_table,0,len(text))
+
+    set = map(lambda t: st_str[t[1]:t[2]], set)
+    qur = map(lambda t: st_str[t[1]:t[2]], qur)
+
+    print set
+
+    return (set,qur)
+
+def varsFromStatements(st_lst):
+    """
+    Input: a list of statements
+    Output: a tuple of set and queried variables
+            (set, queried)
+
+            (['r0','r1'],['r3','r4'])
+    """
+    set = []
+    qur = []
+    for i in st_lst:
+        tmp = varsFromStatement(i)
+        set.extend(tmp[0])
+        qur.extend(tmp[1])
+
+    return (set,qur)
 
 if __name__ == '__main__':
     
@@ -110,19 +145,15 @@ if __name__ == '__main__':
     f = open(sys.argv[1])
     text = f.read()
 
-    t = TextTools._timer()
 
-    t.start()
-    # don't need a taglist, so pass None
-    result, taglist, nextindex = tag(text,tag_table,0,len(text))
+    body_tokens = tokensFromBody(text)
 
-    statement_body = reduce(lambda x,y:x+'\n'+text[y[1]:y[2]],statements,'')
-    
-    st_result, st_taglist, st_nextindex = tag(statement_body,
-        statement_table,0,len(text))
-    t = t.stop()
+    statements = map(lambda x: text[x[1]:x[2]],body_tokens[0])
 
-    print taglist
+    s_q_vars = varsFromStatements(statements)
+
+    set = s_q_vars[0]
+    qur = s_q_vars[1]
 
 #    for i in taglist:
 #        print i,' ', text[i[1]:i[2]]
@@ -148,5 +179,9 @@ if __name__ == '__main__':
 
     print 'set'
     for i in set:
-        print ' ',statement_body[i[1]:i[2]]
+        print ' ' ,i
+    print
+
+    for i in qur:
+        print ' ' ,i
     print
