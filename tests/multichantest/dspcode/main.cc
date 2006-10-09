5016f591201238fe5cce603bdb6fa86b8e02f389
/C=US/ST=Massachusetts/L=Cambridge/O=soma/CN=Eric Jonas/emailAddress=jonas@soma.mit.edu </C=US/ST=Massachusetts/L=Cambridge/O=soma/CN=Eric Jonas/emailAddress=jonas@soma.mit.edu@5dccc5d9-beb7-0310-bf71-cc18d937a3f4>
2006-10-09 14:53:52 +0000

    * Current configuration with 1-D DMA and 16-words per cycle with MFD=1 works, and correctly reads in properly-aligned arrays.
    git-svn-id: file:///home/jonas/soma/svn-restore.20080710/DSPboard/trunk@1762 5dccc5d9-beb7-0310-bf71-cc18d937a3f4

--- tests/multichantest/dspcode/main.cc
+++ tests/multichantest/dspcode/main.cc
@@ -5,7 +5,7 @@
 
 #include <cdefBF533.h>
 
-unsigned short inarray[16]; 
+unsigned short inarray[128]; 
 
 int main()
 {
@@ -13,7 +13,10 @@ int main()
   int k = 0; 
   int a, b, c, d, e, f, g, h; 
 
-
+  // zero memory
+  for (int y = 0 ; y < 128; y++) {
+      inarray[y] = 0; 
+  }
   // first, make sure we can modify the registers
   *pSPORT0_TCR1 = 0x0000; 
   *pSPORT0_RCR1 = 0x0000; 
@@ -28,51 +31,29 @@ int main()
   *pSPORT0_TCR2 = 0x000F; // 16-bit word length
 
   // multichannel
-  *pSPORT0_MCMC1 = 0x0000; // window size of 4 16-bit words
-  *pSPORT0_MRCS0 = 0x000000FF; // enable lower 8 channels. 
+  *pSPORT0_MCMC1 = 0x1000; // window size of 16 words
+  *pSPORT0_MRCS0 = 0x0000FFFF; // enable lower 16 channels. 
   *pSPORT0_MCMC2 = 0x1010; // enable mode
 
   // configure SPI DMA
    *pDMA1_PERIPHERAL_MAP = 0x1000; 
 
    *pDMA1_START_ADDR = &inarray[0]; 
-  *pDMA1_X_COUNT = 8;
-   *pDMA1_X_MODIFY = 0x02; 
-   *pDMA1_Y_COUNT = 0x00; 
-   *pDMA1_Y_MODIFY = 0x00; 
+   *pDMA1_X_COUNT = 128; 
+   *pDMA1_X_MODIFY = 0x02; // two byte stride 
+   *pDMA1_Y_COUNT = 0; // 
+   *pDMA1_Y_MODIFY = 0; // 16*2; // every 32 bytes
    *pDMA1_CURR_DESC_PTR = 0x00; 
 
-
-  // FLOW = 0: stop
-  // DI_EN = 0 : no completion interrupt
-  // DI_SEL = 0 : no completion interrupt
-  // WDSIZE = 01 : 16-bit transfers
-  // enable
-
-
-  for (int p = 0; p < 100000; p++)
-    {
    *pDMA1_CONFIG = 0x0027;  // start dma
 
    *pSPORT0_RCR1 = 0x4001; // enable sport RX
    *pSPORT0_TCR1 = 0x4001; // enable sport TX
 
-      //a = *pSPORT0_RX16; 
-      b = *pDMA1_IRQ_STATUS; 
-      c = *pSPORT0_STAT; 
-      short z = 0; 
-      //z = *pDMA1_START_ADDR; 
-      z = *pDMA1_X_COUNT; 
-      z = *pDMA1_X_MODIFY; 
-      z = *pDMA1_Y_COUNT; 
-      z = *pDMA1_Y_MODIFY; 
-      //z = *pDMA1_CURR_DESC_PTR; 
 
-
-   *pSPORT0_RCR1 = 0x0000; // enable sport RX
-   *pSPORT0_TCR1 = 0x0000; // enable sport TX
-
-
+  for (int p = 0; p < 100000; p++)
+    {
+      i++; 
     }
    
   while(1); 
