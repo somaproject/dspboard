f3724e5a1efd126b84d5a0a9fc77e399d8659645
/C=US/ST=Massachusetts/O=Massachusetts Institute of Technology/OU=Client CA v1/CN=Eric M Jonas/emailAddress=jonas@MIT.EDU </C=US/ST=Massachusetts/O=Massachusetts Institute of Technology/OU=Client CA v1/CN=Eric M Jonas/emailAddress=jonas@MIT.EDU@5dccc5d9-beb7-0310-bf71-cc18d937a3f4>
2006-10-10 12:24:40 +0000

    * Added acqcmdmux docs and code
    git-svn-id: file:///home/jonas/soma/svn-restore.20080710/DSPboard/trunk@1767 5dccc5d9-beb7-0310-bf71-cc18d937a3f4

---
+++ vhdl/acqcmdmux.vhd
@@ -0,0 +1,159 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.STD_LOGIC_ARITH.all;
+use IEEE.STD_LOGIC_UNSIGNED.all;
+
+
+
+entity acqcmdmux is
+  
+  port (
+    CLK : in std_logic;
+    CMDINA : in std_logic_vector(47 downto 0);
+    CMDINB : in std_logic_vector(47 downto 0);
+    NEWCMDS : in std_logic;
+    LINKUP : in std_logic; 
+    CMDOUT : out std_logic(47 downto 0);
+    SENDCMD : out std_logic
+  ); 
+    
+end acqcmdmux;
+
+architecture Behavioral of acqcmdmux is
+  signal cmdinal, cmdinall : std_logic_vector(47 downto 0) := (others => '0');
+  signal cmdinbl, cmdinbll : std_logic_vector(47 downto 0) := (others => '0');
+
+  signal newcmda, newcmdb : std_logic := '0';
+
+  signal newcmdal, newcmdbl : std_logic := '0;
+
+  type states is (none, checka, senda, waita, donea, checkb, sendb, waitb, doneb);
+
+  signal osel : std_logic := '0';
+  
+  signal cs, ns : states := none;
+  
+begin  -- Behavioral
+
+  CMDOUT <= cmdinall when osel = '0' else cmdinbll ;
+  
+  main: process(CLK, LINKUP)
+    begin
+      if LINKUP = '0'  then
+        -- global reset
+        cmdinal <= (others => '0');
+        cmdinbl <= (others => '0');
+        cmdinall <= (others => '0');
+        cmdinbll <= (others => '0');
+        newcmdal <= '0';
+        newcmdbl <= '0';
+        cs <= none; 
+      else
+        if rising_edge(CLK) then
+          ns <= cs;
+
+          if NEWCMDS = '1' then
+            cmdinal <= CMDINA;
+            cmdinbl <= CMDINB; 
+          end if;
+
+          if not (cmdinal(3 downto 0) = X"0"
+            or  cmdinal(7 downto 4) = cmdinall(7 downto 0)) then
+            cmdinall <= cmdinal;
+            newcmdal <= '1';
+          else
+            if cs = donea then
+              newcmdal <= '0'; 
+            end if;
+          end if;
+          
+          if not (cmdinbl(3 downto 0) = X"0"
+            or  cmdinbl(7 downto 4) = cmdinbll(7 downto 0)) then
+            cmdinbll <= cmdinbl;
+            newcmdbl <= '1';
+          else
+            if cs = doneb then
+              newcmdbl <= '0'; 
+            end if;
+          end if;
+          
+        end if;
+      end if;
+
+    end process main; 
+  
+    fsm: process(cs, newcmdal, cmdid, cmdinall, cmdinbll, newcmdbl)
+      begin
+        case cs is
+          when none =>
+            osel <= '0';
+            SENDCMD <= '0';
+            ns <= checka;
+            
+          when checka =>
+            osel <= '0';
+            SENDCMD <= '0';
+            if newcmdal = '1' then
+              ns <= senda;
+            else
+              ns <= checkb; 
+            end if;
+
+          when senda =>
+            osel <= '0';
+            sendcmd <= '1';
+            ns <= waita;
+
+          when waita =>
+            osel <= '0';
+            sendcmd <= '0';
+            if cmdid = cmdinall(7 downto 4)  then
+              ns <= donea;
+            else
+              ns <= waita;
+            end if;
+
+          when donea =>
+            osel <= '0';
+            sendcmd <= '0';
+            ns <= checkb; 
+            
+          when checkb =>
+            osel <= '1';
+            SENDCMD <= '0';
+            if newcmdbl = '1' then
+              ns <= sendb;
+            else
+              ns <= none; 
+            end if;
+
+          when sendb =>
+            osel <= '1';
+            sendcmd <= '1';
+            ns <= waitb;
+
+          when waitb =>
+            osel <= '1';
+            sendcmd <= '0';
+            if cmdid = cmdinbll(7 downto 4)  then
+              ns <= doneb;
+            else
+              ns <= waitb;
+            end if;
+
+          when doneb =>
+            osel <= '1';
+            sendcmd <= '0';
+            ns <= none; 
+            
+          when others =>
+            osel <= '0';
+            sendcmd <= '0';
+            ns <= none; 
+            
+        end case;
+
+      end process fsm;
+
+      
+end Behavioral;
---
+++ vhdl/sportacqser.vhd
@@ -0,0 +1,133 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.STD_LOGIC_ARITH.all;
+use IEEE.STD_LOGIC_UNSIGNED.all;
+
+entity sportacqser is
+
+  port (
+    CLK      : in  std_logic;
+    SERCLK   : out std_logic;
+    SERDT    : out std_logic;
+    SERDRA   : in  std_logic;
+    SERDRB   : in  std_logic;
+    START    : in  std_logic;
+    DONE     : out std_logic;
+    DATAIN   : in  std_logic_vector(255 downto 0);
+    DATAOUTA : out std_logic_vector(255 downto 0);
+    DATAOUTB : out std_logic_vector(255 downto 0)
+    );
+
+end sportacqser;
+
+architecture Behavioral of sportacqser is
+
+  signal dinsreg          : std_logic_vector(255 downto 0) := (others => '0');
+  signal lsclk, lfs       : std_logic                      := '0';
+  signal serdral, serdrbl : std_logic                      := '0';
+
+  signal inenl, inenll : std_logic := '0';
+
+  signal bitpos : std_logic_vector(7 downto 0) := (others => '0');
+
+  type states is (none, tfs1, tfs2, clkl, clkh, dones);
+  signal cs, ns : states := none;
+
+
+begin  -- Behavioral
+
+  DATAOUTA <= dataoutaint;
+  DATAOUTB <= dataoutbint;
+
+  main : process(CLK)
+  begin
+    if rising_edge(CLK) then
+      cs <= ns;
+
+      -- outputs
+      SERDT  <= dinsreg(0);
+      SERCLK <= lsclk;
+      SERFS  <= lfs;
+
+      serdral <= SERDRA;
+      serdrbl <= SERDRB;
+
+      if inenll = '1' then
+        dataoutaint <= dataoutaint(254 downto 0) & serdral;
+        dataoutbint <= dataoutbint(254 downto 0) & serdrbl;
+      end if;
+
+      if cs = none and START = '1' then
+        dinsreg   <= DATAIN;
+      else
+        if cs = clkh then
+          dinsreg <= '0' &  dinsreg(255 downto 1); 
+        end if;
+      end if;
+
+
+      ifs cs = clkh then
+        bitpos <= bitpos + 1;
+      end if;
+
+      if cs = clkh then
+        inenl <= '1';
+      else
+        inenl <= '0';
+      end if;
+
+      inenll <= inenl;
+
+    end if;
+  end process main;
+
+  fsm : process(cs, START, bitpos)
+  begin
+    case cs is
+      when none =>
+        lsclk <= '0';
+        lfs   <= '0';
+        if START = '1' then
+          ns  <= tfs1;
+        else
+          ns  <= none;
+        end if;
+
+      when tfs1 =>
+        lsclk <= '0';
+        lfs   <= '1';
+        ns    <= tfs2;
+
+      when tfs2 =>
+        lsclk <= '1';
+        lfs   <= '1';
+        ns    <= clkl;
+
+      when clkl =>
+        lsclk <= '0';
+        lfs   <= '0';
+        ns    <= clkh;
+
+      when clkh =>
+        lsclk <= '1';
+        lfs   <= '0';
+
+        if bitpos = X"FF" then
+          ns <= dones;
+        else
+          ns <= clkl;
+        end if;
+
+      when dones =>
+        lsclk <= '0';
+        lfs   <= '0';
+        ns    <= none;
+
+      when others =>
+        lsclk <= '0';
+        lfs   <= '0';
+        ns    <= none;
+    end case;
+  end process fsm;
+
+end Behavioral;
