dff6f351f3b33cd20f792f8ec0142fe048032e21
/C=US/ST=Massachusetts/L=Cambridge/O=soma/CN=Eric Jonas/emailAddress=jonas@soma.mit.edu </C=US/ST=Massachusetts/L=Cambridge/O=soma/CN=Eric Jonas/emailAddress=jonas@soma.mit.edu@5dccc5d9-beb7-0310-bf71-cc18d937a3f4>
2006-10-25 14:49:38 +0000

    * working sport test, where we trigger the FS constantly
    git-svn-id: file:///home/jonas/soma/svn-restore.20080710/DSPboard/trunk@1803 5dccc5d9-beb7-0310-bf71-cc18d937a3f4

--- tests/datasporttest/dspcode/main.cc
+++ tests/datasporttest/dspcode/main.cc
@@ -5,7 +5,39 @@
 
 #include <cdefBF533.h>
 
-unsigned short inarray[16][16]; 
+unsigned char outarray[600]; 
+
+void sendSPORTBurst(unsigned char * x) 
+{
+  // first, make sure we can modify the registers
+  *pSPORT0_TCR1 = 0x0000; 
+
+  *pSPORT0_TCR2 = 0x0007; // 8-bit word length
+  *pSPORT0_MCMC2  = 0x0000; // make sure multichannel mode is disabled
+
+
+  // configure SPI DMA
+  *pDMA2_PERIPHERAL_MAP = 0x2000; 
+  
+  *pDMA2_START_ADDR = x; 
+  *pDMA2_X_COUNT = 600; 
+  *pDMA2_X_MODIFY = 0x01; //  one-byte stride
+  *pDMA2_Y_COUNT = 0; // 
+  *pDMA2_Y_MODIFY = 0; // 
+  *pDMA2_CURR_DESC_PTR = 0x00; 
+  
+  *pDMA2_CONFIG = 0x0001;  // start dma, 2D
+  
+  *pSPORT0_TFSDIV = 0x0000; 
+  *pSPORT0_TCR1 = 0x4211; // enable sport TX// TEST TO USE TFS ON EACH FRAME
+
+  
+
+  for (int p = 0; p < 100000; p++)
+    {
+      p++; 
+    }
+}
 
 int main()
 {
@@ -15,50 +47,18 @@ int main()
 
   // zero memory
 
-  for (int y = 0 ; y < 16; y++) {
-    for (int x = 0; x < 16; x++) {
-      inarray[y][x] = 0; 
-    }
+  for (int y = 0 ; y < 600; y++) {
+    outarray[y] = y % 256; 
   }
-  // first, make sure we can modify the registers
-  *pSPORT0_TCR1 = 0x0000; 
-  *pSPORT0_RCR1 = 0x0000; 
 
-// enable settings
+  sendSPORTBurst(outarray) ; 
+    
+  for (int y = 0 ; y < 600; y++) {
+    outarray[y] = (2*y) % 256; 
+  }
+  sendSPORTBurst(outarray) ; 
+    
 
-  // require RFS for every word
-  // active high RFS
-  // rising-edge of RSCLK to sample
-
-  *pSPORT0_RCR2 = 0x000F; // 16-bit word length
-  *pSPORT0_TCR2 = 0x000F; // 16-bit word length
-
-  // multichannel
-  *pSPORT0_MCMC1 = 0x1000; // window size of 16 words
-  *pSPORT0_MRCS0 = 0x0000FFFF; // 
-  *pSPORT0_MRCS1 = 0x00000000; //
-  *pSPORT0_MCMC2 = 0x1010; // enable mode
-
-  // configure SPI DMA
-   *pDMA1_PERIPHERAL_MAP = 0x1000; 
-
-   *pDMA1_START_ADDR = &inarray[0]; 
-   *pDMA1_X_COUNT = 16; 
-   *pDMA1_X_MODIFY = 0x02; // two byte stride 
-   *pDMA1_Y_COUNT = 16; // 
-   *pDMA1_Y_MODIFY = 2; // 
-   *pDMA1_CURR_DESC_PTR = 0x00; 
-
-   *pDMA1_CONFIG = 0x0037;  // start dma, 2D
-
-   *pSPORT0_RCR1 = 0x4001; // enable sport RX
-   *pSPORT0_TCR1 = 0x4001; // enable sport TX
-
-
-  for (int p = 0; p < 100000; p++)
-    {
-      i++; 
-    }
    
   while(1); 
 
