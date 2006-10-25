b9a86a806c026c10097ffe3ebf8780b4531f4d2a
/C=US/ST=Massachusetts/L=Cambridge/O=soma/CN=Eric Jonas/emailAddress=jonas@soma.mit.edu </C=US/ST=Massachusetts/L=Cambridge/O=soma/CN=Eric Jonas/emailAddress=jonas@soma.mit.edu@5dccc5d9-beb7-0310-bf71-cc18d937a3f4>
2006-10-25 14:50:02 +0000

    * working data sport test
    git-svn-id: file:///home/jonas/soma/svn-restore.20080710/DSPboard/trunk@1804 5dccc5d9-beb7-0310-bf71-cc18d937a3f4

--- tests/datasporttest/vhdl/datasporttest.vhd
+++ tests/datasporttest/vhdl/datasporttest.vhd
@@ -14,6 +14,7 @@ entity datasporttest is
     DSPRESET  : out std_logic;
     LEDPOWER  : out std_logic;
     RSCLK     : out std_logic;
+    TSCLK     : out std_logic;
     DR        : out std_logic;
     RFS       : out std_logic;
     DT        : in  std_logic;
@@ -44,14 +45,14 @@ architecture Behavioral of datasporttest is
       FIFONEXT : in  std_logic);
   end component;
 
-  signal valid    : std_logic := '0';
+  signal valid    : std_logic                    := '0';
   signal dout     : std_logic_vector(7 downto 0) := (others => '0');
   signal addr     : std_logic_vector(9 downto 0) := (others => '0');
-  signal fifonext : std_logic := '0';
-  signal f1, f2 : std_logic := '0';
-  
+  signal fifonext : std_logic                    := '0';
+  signal f1, f2   : std_logic                    := '0';
 
-  
+
+
   signal jtagcapture : std_logic := '0';
   signal jtagdrck1   : std_logic := '0';
   signal jtagdrck2   : std_logic := '0';
@@ -63,10 +64,12 @@ architecture Behavioral of datasporttest is
   signal jtagtdo2    : std_logic := '0';
   signal jtagupdate  : std_logic := '0';
 
-  signal jtagout : std_logic_vector(15 downto 0) := (others => '0');
-  signal jtagin : std_logic_vector(15 downto 0) := (others => '0'); 
+  signal jtagout : std_logic_vector(23 downto 0) := (others => '0');
+  signal jtagin  : std_logic_vector(15 downto 0) := (others => '0');
 
+  signal tfscnt : std_logic_vector(7 downto 0) := (others => '0');
   
+
 begin  -- Behavioral
 
 
@@ -74,32 +77,33 @@ begin  -- Behavioral
 
   DSPCLKOUT <= clk;
 
-  DR <= '0';
-  RFS <= '0';
+  DR       <= '0';
+  RFS      <= '0';
   DSPRESET <= '1';
-  
-  datasportinst: datasport
-    port map (
-      CLK   => CLK,
-      reset => '0',
-      SDIN => DT,
-      STFS => TFS,
-      SCLK => sclktoggle,
-      VALID => valid,
-      dout => dout,
-      ADDRIN => addr,
-      fifonext => fifonext
-      ); 
 
-    
+  datasportinst : datasport
+    port map (
+      CLK      => CLK,
+      reset    => '0',
+      SDIN     => DT,
+      STFS     => TFS,
+      SCLK     => sclktoggle,
+      VALID    => valid,
+      dout     => dout,
+      ADDRIN   => addr,
+      fifonext => fifonext
+      );
+
+
   main : process (CLK)
   begin
     if rising_edge(CLK) then
       sclktoggle <= not sclktoggle;
       RSCLK      <= sclktoggle;
-
+      TSCLK <= sclktoggle;
+      
       if jtagupdate = '1' then
-        f1 <= jtagin(15);                         
+        f1 <= jtagin(15);
       end if;
 
       f2 <= f1;
@@ -107,30 +111,31 @@ begin  -- Behavioral
       if f1 = '1' and f2 = '0' then
         fifonext <= '1';
       else
-        fifonext <= '0'; 
+        fifonext <= '0';
       end if;
-      LEDPOWER <= fifonext;
-      
-      -- capture
-      --
-      --
-      
+      LEDPOWER   <= fifonext;
+
+
+      if sclktoggle = '1' and  tfs= '1'  then
+        tfscnt <= tfscnt + 1;
+      end if;
+
     end if;
   end process main;
 
   process(jtagDRCK1, clk, jtagupdate, jtagsel1, jtagsel2)
   begin
 
-    if jtagupdate = '1' and jtagsel1 = '1'  then
-      jtagout   <= X"AB" & dout;
-      addr <= jtagin(9 downto 0); 
+    if jtagupdate = '1' and jtagsel1 = '1' then
+      jtagout    <= "0000000" & valid & tfscnt & dout;
+      addr       <= jtagin(9 downto 0);
     else
-      if rising_edge(jtagDRCK1) then 
-        jtagout <= '0' & jtagout(15 downto 1);
-        jtagtdo1      <= jtagout(0);
+      if rising_edge(jtagDRCK1) then
+        jtagout  <= '0' & jtagout(23 downto 1);
+        jtagtdo1 <= jtagout(0);
 
         jtagin <= (jtagTDI & jtagin(15 downto 1));
-        
+
       end if;
 
     end if;
@@ -148,7 +153,7 @@ begin  -- Behavioral
       TDI     => jtagTDI,               -- TDI output from TAP controller
       UPDATE  => jtagUPDATE,            -- UPDATE output from TAP controller
       TDO1    => jtagtdo1,              -- Data input for USER1 function
-      TDO2    => jtagtdo2             -- Data input for USER2 function
+      TDO2    => jtagtdo2               -- Data input for USER2 function
       );
 
 end Behavioral;
