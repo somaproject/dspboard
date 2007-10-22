e3a5cb041799bab8fa38d3910184a660da28508a
/C=US/ST=Massachusetts/O=Massachusetts Institute of Technology/OU=Client CA v1/CN=Eric M Jonas/emailAddress=jonas@MIT.EDU </C=US/ST=Massachusetts/O=Massachusetts Institute of Technology/OU=Client CA v1/CN=Eric M Jonas/emailAddress=jonas@MIT.EDU@5dccc5d9-beb7-0310-bf71-cc18d937a3f4>
2007-10-22 12:43:33 +0000

    * created merged BOM
    git-svn-id: file:///home/jonas/soma/svn-restore.20080710/DSPboard/trunk@2446 5dccc5d9-beb7-0310-bf71-cc18d937a3f4

--- tests/deviceloopback/dlloop.vhd
+++ tests/deviceloopback/dlloop.vhd
@@ -9,16 +9,16 @@ use UNISIM.VComponents.all;
 
 entity dlloop is
   port (
-    REFCLKIN : in std_logic; 
-    REFCLKOUT : out std_logic; 
-    RXCLKIN : in std_logic; 
-    RXLOCKED : in  std_logic;
-    RXDIN    : in  std_logic_vector(9 downto 0);
-    TXIO_P   : out std_logic;
-    TXIO_N   : out std_logic;
-    LEDPOWER : out std_logic;
+    REFCLKIN  : in  std_logic;
+    REFCLKOUT : out std_logic;
+    RXCLKIN   : in  std_logic;
+    RXLOCKED  : in  std_logic;
+    RXDIN     : in  std_logic_vector(9 downto 0);
+    TXIO_P    : out std_logic;
+    TXIO_N    : out std_logic;
+    LEDPOWER  : out std_logic;
     LEDLOCKED : out std_logic;
-    LEDVALID : out std_logic;
+    LEDVALID  : out std_logic;
     DECODEERR : out std_logic
     );
 
@@ -28,64 +28,64 @@ architecture Behavioral of dlloop is
 
   component devicelink
     port (
-      TXCLKIN  : in  std_logic;
-      TXLOCKED : in  std_logic;
-      TXDIN    : in  std_logic_vector(9 downto 0);
-      TXDOUT   : out std_logic_vector(7 downto 0);
-      TXKOUT   : out std_logic;
-      CLK      : out std_logic;
-      CLK2X    : out std_logic;
-      RESET    : out std_logic;
-      RXDIN    : in  std_logic_vector(7 downto 0);
-      RXKIN    : in  std_logic;
-      RXIO_P   : out std_logic;
-      RXIO_N   : out std_logic;
+      TXCLKIN   : in  std_logic;
+      TXLOCKED  : in  std_logic;
+      TXDIN     : in  std_logic_vector(9 downto 0);
+      TXDOUT    : out std_logic_vector(7 downto 0);
+      TXKOUT    : out std_logic;
+      CLK       : out std_logic;
+      CLK2X     : out std_logic;
+      RESET     : out std_logic;
+      RXDIN     : in  std_logic_vector(7 downto 0);
+      RXKIN     : in  std_logic;
+      RXIO_P    : out std_logic;
+      RXIO_N    : out std_logic;
       DECODEERR : out std_logic
       );
 
   end component;
 
   signal valid : std_logic := '0';
-  
-  signal data : std_logic_vector(7 downto 0) := (others => '0');
-  signal k : std_logic := '0';
-  signal clk, clk2x : std_logic := '0';
-  signal RESET : std_logic := '0';
-  
-  signal pcnt : std_logic_vector(21 downto 0) := (others => '0');
-  signal decodeerrint : std_logic := '0';
 
-  
+  signal data       : std_logic_vector(7 downto 0) := (others => '0');
+  signal k          : std_logic                    := '0';
+  signal clk, clk2x : std_logic                    := '0';
+  signal RESET      : std_logic                    := '0';
+
+  signal pcnt         : std_logic_vector(21 downto 0) := (others => '0');
+  signal decodeerrint : std_logic                     := '0';
+
+
 begin  -- Behavioral
 
-  
-  devicelink_inst: devicelink
+
+  devicelink_inst : devicelink
     port map (
-      TXCLKIN  => RXCLKIN,
-      TXLOCKED => RXLOCKED,
-      TXDIN    => RXDIN,
-      TXDOUT   => data,
-      TXKOUT   => k, 
-      CLK      => clk,
-      CLK2X    => clk2x,
-      RESET    => RESET,
-      RXDIN    => data,
-      RXKIN    => '0', 
-      RXIO_P   => TXIO_P,
-      RXIO_N   => TXIO_N,
+      TXCLKIN   => RXCLKIN,
+      TXLOCKED  => RXLOCKED,
+      TXDIN     => RXDIN,
+      TXDOUT    => data,
+      TXKOUT    => k,
+      CLK       => clk,
+      CLK2X     => clk2x,
+      RESET     => RESET,
+      RXDIN     => data,
+      RXKIN     => k,
+      RXIO_P    => TXIO_P,
+      RXIO_N    => TXIO_N,
       DECODEERR => decodeerrint);
-  
-  ledpowerproc: process (clk)
+
+  ledpowerproc : process (clk)
   begin  -- process ledpowerproc
     if rising_edge(clk) then
-      pcnt <= pcnt + 1;
-      LEDPOWER <= pcnt(21);
-      LEDVALID <= not decodeerrint;
+      pcnt      <= pcnt + 1;
+      LEDVALID  <=  not decodeerrint;
+      LEDPOWER  <= pcnt(21);
       DECODEERR <= decodeerrint;
     end if;
   end process ledpowerproc;
 
-  REFCLKOUT <= REFCLKIN; 
+  REFCLKOUT <= REFCLKIN;
   LEDLOCKED <= not RXLOCKED;
-  
+
 end Behavioral;
