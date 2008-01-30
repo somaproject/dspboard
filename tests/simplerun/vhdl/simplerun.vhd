5854f8ed8bfa7a73b068b99c6310c1aac87a4f80
/C=US/ST=Massachusetts/O=Massachusetts Institute of Technology/OU=Client CA v1/CN=Eric M Jonas/emailAddress=jonas@MIT.EDU </C=US/ST=Massachusetts/O=Massachusetts Institute of Technology/OU=Client CA v1/CN=Eric M Jonas/emailAddress=jonas@MIT.EDU@5dccc5d9-beb7-0310-bf71-cc18d937a3f4>
2008-01-30 21:55:55 +0000

    update to the blink example for booting
    git-svn-id: file:///home/jonas/soma/svn-restore.20080710/DSPboard/trunk@2564 5dccc5d9-beb7-0310-bf71-cc18d937a3f4

--- tests/simplerun/vhdl/simplerun.vhd
+++ tests/simplerun/vhdl/simplerun.vhd
@@ -9,44 +9,73 @@ use UNISIM.VComponents.all;
 
 entity simplerun is
   port (
-    REFCLKIN : in std_logic;
+    SYSCLKIN  : in  std_logic;
     DSPCLKOUT : out std_logic;
-    DSPRESET : out std_logic;
-    LEDPOWER : out std_logic);
+    DSPRESET  : out std_logic;
+    LEDPOWER  : out std_logic;
+    LEDEVENTA : out std_logic;
+    LEDEVENTB : out std_logic;
+    LEDEVENTC : out std_logic;
+    LEDEVENTD : out std_logic
+    );
 end simplerun;
 
 
 architecture Behavioral of simplerun is
 
-  signal clk : std_logic := '0';
+  signal clk        : std_logic                     := '0';
   signal dspcntdown : std_logic_vector(24 downto 0) := (others => '1');
-  signal cnt : std_logic_vector(21 downto 0) := (others => '0');
-  signal ldspreset : std_logic := '0';
-  
-  
+  signal cnt        : std_logic_vector(23 downto 0) := (others => '0');
+  signal ldspreset  : std_logic                     := '0';
+
+
 begin  -- Behavioral
 
-  clk <= REFCLKIN;
+  clk       <= SYSCLKIN;
   DSPCLKOUT <= clk;
 
-  main: process(clk)
-    begin
-      if rising_edge(clk) then
+  main : process(clk)
+  begin
+    if rising_edge(clk) then
 
-        DSPRESET <= ldspreset;
-        
-        if dspcntdown /= "0000000000000000000000000" then
-          dspcntdown <= dspcntdown - 1;
-        else
-          ldspreset <= '1'; 
-        end if;
-        
-        cnt <= cnt + 1;
+      DSPRESET <= ldspreset;
 
-        LEDPOWER <= cnt(21);
-        
+      if dspcntdown /= "0000000000000000000000000" then
+        dspcntdown <= dspcntdown - 1;
+      else
+        ldspreset  <= '1';
       end if;
-    end process main; 
-  
+
+      cnt <= cnt + 1;
+
+      LEDPOWER <= cnt(21);
+      
+      if cnt(23 downto 22) = "00" then
+        LEDEVENTA <= '1';
+      else
+        LEDEVENTA <= '0';
+      end if;
+
+      if cnt(23 downto 22) = "01" then
+        LEDEVENTB <= '1';
+      else
+        LEDEVENTB <= '0';
+      end if;
+
+      if cnt(23 downto 22) = "10" then
+        LEDEVENTC <= '1';
+      else
+        LEDEVENTC <= '0';
+      end if;
+
+      if cnt(23 downto 22) = "11" then
+        LEDEVENTD <= '1';
+      else
+        LEDEVENTD <= '0';
+      end if;
+
+    end if;
+  end process main;
+
 
 end Behavioral;
