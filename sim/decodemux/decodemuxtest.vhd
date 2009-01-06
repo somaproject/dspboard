6f0dedf3bf84955bdb57a81222ad25813e502d0d
Eric Jonas <jonas@mit.edu>
2009-01-06 14:18:47 -0500

    decodemux test now works again

--- sim/decodemux/decodemuxtest.vhd
+++ sim/decodemux/decodemuxtest.vhd
@@ -10,31 +10,53 @@ architecture Behavioral of decodemuxtest is
 
   component decodemux
     port (
-      CLK     : in  std_logic;
-      DIN     : in  std_logic_vector(7 downto 0);
-      KIN     : in  std_logic;
-      LOCKED  : in  std_logic;
-      ECYCLE  : out std_logic;
-      EDATA   : out std_logic_vector(7 downto 0);
+      CLK    : in std_logic;
+      DIN    : in std_logic_vector(7 downto 0);
+      KIN    : in std_logic;
+      LOCKED : in std_logic;
+
+      ECYCLE       : out std_logic;
+      EDATA        : out std_logic_vector(7 downto 0);
       -- data interface
-      DGRANTA : out std_logic;
-      EARXA   : out std_logic_vector(79 downto 0);
-      DGRANTB : out std_logic;
-      EARXB   : out std_logic_vector(79 downto 0);
-      DGRANTC : out std_logic;
-      EARXC   : out std_logic_vector(79 downto 0);
-      DGRANTD : out std_logic;
-      EARXD   : out std_logic_vector(79 downto 0)
+      DGRANTA      : out std_logic;
+      EARXBYTEA    : out std_logic_vector(7 downto 0) := (others => '0');
+      EARXBYTESELA : in  std_logic_vector(3 downto 0) := (others => '0');
+
+      DGRANTB      : out std_logic;
+      EARXBYTEB    : out std_logic_vector(7 downto 0) := (others => '0');
+      EARXBYTESELB : in  std_logic_vector(3 downto 0) := (others => '0');
+
+      DGRANTC      : out std_logic;
+      EARXBYTEC    : out std_logic_vector(7 downto 0) := (others => '0');
+      EARXBYTESELC : in  std_logic_vector(3 downto 0) := (others => '0');
+
+      DGRANTD      : out std_logic;
+      EARXBYTED    : out std_logic_vector(7 downto 0) := (others => '0');
+      EARXBYTESELD : in  std_logic_vector(3 downto 0) := (others => '0')
+
       );
   end component;
 
-  signal CLK     : std_logic                     := '0';
-  signal DIN     : std_logic_vector(7 downto 0)  := (others => '0');
-  signal KIN     : std_logic                     := '0';
-  signal LOCKED  : std_logic                     := '1';
-  signal ECYCLE  : std_logic                     := '0';
-  signal EDATA   : std_logic_vector(7 downto 0)  := (others => '0');
+  signal CLK    : std_logic                    := '0';
+  signal DIN    : std_logic_vector(7 downto 0) := (others => '0');
+  signal KIN    : std_logic                    := '0';
+  signal LOCKED : std_logic                    := '1';
+  signal ECYCLE : std_logic                    := '0';
+  signal EDATA  : std_logic_vector(7 downto 0) := (others => '0');
   -- data interface
+
+  signal EARXBYTEA    : std_logic_vector(7 downto 0) := (others => '0');
+  signal EARXBYTESELA : std_logic_vector(3 downto 0) := (others => '0');
+
+  signal EARXBYTEB    : std_logic_vector(7 downto 0) := (others => '0');
+  signal EARXBYTESELB : std_logic_vector(3 downto 0) := (others => '0');
+
+  signal EARXBYTEC    : std_logic_vector(7 downto 0) := (others => '0');
+  signal EARXBYTESELC : std_logic_vector(3 downto 0) := (others => '0');
+
+  signal EARXBYTED    : std_logic_vector(7 downto 0) := (others => '0');
+  signal EARXBYTESELD : std_logic_vector(3 downto 0) := (others => '0');
+
   signal DGRANTA : std_logic                     := '0';
   signal EARXA   : std_logic_vector(79 downto 0) := (others => '0');
   signal DGRANTB : std_logic                     := '0';
@@ -54,20 +76,25 @@ begin  -- Behavioral
 
   decodemux_inst : decodemux
     port map (
-      CLK     => CLK,
-      DIN     => DIN,
-      KIN     => KIN,
-      LOCKED  => LOCKED,
-      ECYCLE  => ECYCLE,
-      EDATA   => EDATA,
-      DGRANTA => DGRANTA,
-      EARXA   => EARXA,
-      DGRANTB => DGRANTB,
-      EARXB   => EARXB,
-      DGRANTC => DGRANTC,
-      EARXC   => EARXC,
-      DGRANTD => DGRANTD,
-      EARXD   => EARXD);
+      CLK          => CLK,
+      DIN          => DIN,
+      KIN          => KIN,
+      LOCKED       => LOCKED,
+      ECYCLE       => ECYCLE,
+      EDATA        => EDATA,
+      DGRANTA => DGRANTA, 
+      EARXBYTEA    => EARXBYTEA,
+      EARXBYTESELA => EARXBYTESELA,
+      DGRANTB => DGRANTB, 
+      EARXBYTEB    => EARXBYTEB,
+      EARXBYTESELB => EARXBYTESELB,
+      DGRANTC => DGRANTC, 
+      EARXBYTEC    => EARXBYTEC,
+      EARXBYTESELC => EARXBYTESELC,
+      DGRANTD => DGRANTD, 
+      EARXBYTED    => EARXBYTED,
+      EARXBYTESELD => EARXBYTESELD);
+
 
   ecycle_generation : process(CLK)
   begin
@@ -95,6 +122,7 @@ begin  -- Behavioral
         wait until rising_edge(CLK);
         KIN   <= '0';
         DIN   <= std_logic_vector(TO_UNSIGNED(i + pkt, 8));
+        --now the data
         for j in 0 to 9 loop
           wait until rising_edge(CLK);
           KIN <= '0';
@@ -117,13 +145,31 @@ begin  -- Behavioral
   -- validate output
   process
   begin
-    wait until rising_edge(CLK) and ECYCLE = '1';
     for pkt in 0 to 7 loop
       wait until rising_edge(CLK) and ECYCLE = '1';
+    --   wait until the pos is such that we've clearly had time to read out
+    -- all of the ecycle data
+      wait until rising_edge(CLK) and pos = 50;
+      -- extract and save all of the ecycle data
+      for j in 0 to 9 loop
+        EARXBYTESELA <= std_logic_vector(to_unsigned(j, 4)); 
+        EARXBYTESELB <= std_logic_vector(to_unsigned(j, 4)); 
+        EARXBYTESELC <= std_logic_vector(to_unsigned(j, 4)); 
+        EARXBYTESELD <= std_logic_vector(to_unsigned(j, 4));
+        wait until rising_edge(CLK);
+        EARXA(j*8 + 7 downto j*8) <= EARXBYTEA;
+        EARXB(j*8 + 7 downto j*8) <= EARXBYTEB;
+        EARXC(j*8 + 7 downto j*8) <= EARXBYTEC;
+        EARXD(j*8 + 7 downto j*8) <= EARXBYTED;
+      
+      end loop;  -- j
+      wait until rising_edge(CLK);
+      wait until rising_edge(CLK);
+      wait until rising_edge(CLK);
       for j in 0 to 9 loop
         assert EARXA(j*8 + 7 downto j*8) =
           std_logic_vector(TO_UNSIGNED(j + 0 + pkt, 8))
-          report "error reading EARXA" severity error;
+          report "error reading EARXA, expecting" & integer'image(j+0+pkt)  severity error;
 
         assert EARXB(j*8 + 7 downto j*8) =
           std_logic_vector(TO_UNSIGNED(j + 20 + pkt, 8))
@@ -140,7 +186,7 @@ begin  -- Behavioral
         assert "0000000" & DGRANTA = std_logic_vector(TO_UNSIGNED(pkt mod 2, 8))
 
           report "Error with DGRANTA" severity error;
-        
+
       end loop;  -- j
     end loop;  -- pkt
     report "End of Simulation" severity failure;
