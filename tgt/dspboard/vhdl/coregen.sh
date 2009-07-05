0b6b4ef43e633e30d263bae3d8c45511481b5051
Eric Jonas <jonas@mit.edu>
2009-07-04 20:40:33 -0400

    moving to different xco layout

--- tgt/dspboard/vhdl/coregen.sh
+++ tgt/dspboard/vhdl/coregen.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-
+coregen -b ../../../vhdl/fiberencode8b10b.xco
+coregen -b ../../../vhdl/fiberdecode8b10b.xco
 coregen -b ../../../vhdl/serial-deviceio/vhdl/encode8b10b.xco
 coregen -b ../../../vhdl/serial-deviceio/vhdl/decode8b10b.xco
