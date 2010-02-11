9913a48cc2cdcf295f3a1a043ce5770a654d39a4
Eric Jonas <jonas@mit.edu>
2010-02-10 21:36:02 -0500

    acqdatasourcecontrol takes in somaip

--- tgt/dspboard/acqdatasourcecontrol.py
+++ tgt/dspboard/acqdatasourcecontrol.py
@@ -6,9 +6,11 @@ from somapynet.neteventio import NetEventIO
 import struct
 import time
 
-eio = NetEventIO("10.0.0.2")
+somaip = sys.argv[1]
 
-DSPBOARDADDR = int(sys.argv[1])
+eio = NetEventIO(somaip)
+
+DSPBOARDADDR = int(sys.argv[2])
 
 eio.addRXMask(xrange(256), DSPBOARDADDR)
 
@@ -43,7 +45,7 @@ e = Event()
 e.src = eaddr.NETWORK
 e.cmd =  0x41
 e.data[0] = 2
-e.data[1] = 0xF # all four channels
+e.data[1] = 0x1F # all five channels
 e.data[2] = 00  # gain = 100
 e.data[3] = 100
 
@@ -55,12 +57,36 @@ eio.sendEvent(ea, e)
 
 print "Gain set event sent"
 eventcnt = 0
-while eventcnt < 12:
+while eventcnt < 5*3:
     erx = eio.getEvents()
     for q in erx:
         print q
         eventcnt += 1
     
+# now we send the chan sel signal
+
+e = Event()
+e.src = eaddr.NETWORK
+e.cmd =  0x41
+e.data[0] = 4 # chansel
+e.data[1] = 3 # tgt chan
+
+
+
+ea = eaddr.TXDest()
+ea[DSPBOARDADDR] = 1
+
+eio.sendEvent(ea, e)
+
+print "chan sel set event sent"
+eventcnt = 0
+while eventcnt < 1:
+    erx = eio.getEvents()
+    for q in erx:
+        print q
+        eventcnt += 1
+
+
 
 eio.stop()
 
