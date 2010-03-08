c98cb8fdcd17fdc8d2e0077dd937d898f91b4b14
Eric Jonas <jonas@mit.edu>
2010-03-08 17:28:03 -0500

    Rebuild of firmware, from hamiltonian

--- tgt/dspboard/dsp/ver_raw.h
+++ tgt/dspboard/dsp/ver_raw.h
@@ -6,8 +6,8 @@ namespace dspboard {
 const uint16_t VERSION_MAJOR = 1; 
 const uint16_t VERSION_MINOR = 0; 
 const char * FIRMWARENAME = "     RAW"; 
-const uint64_t GITHASH = 17160523273994346175LLU; 
-const uint32_t BUILDTIME = 1265851470; 
+const uint64_t GITHASH = 13143919925529823915LLU; 
+const uint32_t BUILDTIME = 1268087230; 
 
 }
 
